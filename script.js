aed2065cdc765d012c0e683409f927cc7f080796
abelbel28 <141388290+abelbel28@users.noreply.github.com>
2026-06-03 13:18:17 +0700

    Update script.js

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function showMessage(option) {
     clearInterval(slideInterval);
     message.innerHTML = `
       🌸 Selamat ulang tahun ya sayang! Semoga hari ini dipenuhi dengan kebahagiaan dan kejutan indah.<br><br>
-      Di hari spesial ini untuk seseorang yang begitu luar biasa dan selalu membawa kebahagiaan dalam hidupku. 
-      Kebaikan hati, tawamu, dan dukungan abang selama ini sangat berarti. Terima kasih ya sayang sudah menjadi bagian terpenting dalam hidup adek.<br><br>
+      Di hari spesial ini untuk abang yang sangat hebat dan selalu membawa kebahagiaan dalam hidup adek. 
+      Kebaikan hati serta dukungan abang selama ini sangat berarti. Terima kasih ya sayang sudah menjadi bagian terpenting dalam hidup adek.<br><br>
       Semoga bertambah usia abang ini, semua impian tercapai, selalu dipermudah di setiap langkah, 
       dan kita bisa terus bersama  dengan momen-momen indah bersama. ❤️<br><br>
       I love you more than words can express!
