0050c5132aa5650827bf06e5edb1674d1cfc18d9
abelbel28 <141388290+abelbel28@users.noreply.github.com>
2026-06-03 08:20:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function playMusic() {
+  let audio = new Audio('happybirthday.mp3'); // tambahkan file musik di folder yang sama
+  audio.play();
+  alert("🎶 Selamat menikmati musik ulang tahun!");
+}
