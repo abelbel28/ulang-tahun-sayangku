9ca5c59b50f54c28d61f2e747bbd367b6ea121ae
abelbel28 <141388290+abelbel28@users.noreply.github.com>
2026-06-19 14:37:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -22,9 +22,9 @@ function showMessage(option) {
       Di hari spesial ini untuk abang yang sangat hebat dan selalu membawa kebahagiaan dalam hidup adek. 
       Kebaikan hati serta dukungan abang selama ini sangat berarti. Terima kasih ya sayang sudah menjadi bagian terpenting dalam hidup adek.<br><br>
       Semoga bertambah usia abang ini, semua impian tercapai, selalu dipermudah di setiap langkah, 
-      dan kita bisa terus bersama  dengan momen-momen indah bersama. ❤️<br><br>
+      dan kita bisa terus bersama dengan momen-momen indah bersama. ❤️<br><br>
       I love you more than words can express!
-    
+    `;
   } else if (option === 2) {
     clearInterval(slideInterval);
     message.innerHTML = "🙏 Semoga Allah selalu memberkahi langkahmu, memberi kesehatan, rezeki, dan kebahagiaan sepanjang hidup.";
